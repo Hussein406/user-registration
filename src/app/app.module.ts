1c028f441306d6814f6d7f32bc58127c20465386
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2023-07-28 11:06:09 -0300

    registration form is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,36 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LoginPageComponent],
+  declarations: [
+    AppComponent,
+    RegistrationPageComponent,
+    NavbarComponent,
+    LoginPageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
 
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
-  styleUrls: ['./registration-page.component.css']
+  styleUrls: ['./registration-page.component.css'],
 })
-export class RegistrationPageComponent {
+export class RegistrationPageComponent implements OnInit {
+  registrationForm!: FormGroup;
+  hide = true;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      bio: [''],
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form submitted:', this.registrationForm.value);
+    }
+  }
 }
