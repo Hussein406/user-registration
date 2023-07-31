b958577a77a39a4eb0acbb2e6cd567bcb6148714
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2023-07-30 23:25:04 -0300

    authenticaiton working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-
   { path: '', component: RegistrationPageComponent },
   { path: 'registration', component: RegistrationPageComponent },
-  { path: 'profile', component: ProfilePageComponent },
+  {
+    path: 'profile',
+    canActivate: [AuthGuard],
+    component: ProfilePageComponent,
+  },
   { path: 'login', component: LoginPageComponent },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   userAuthenticated = false;
+
+  constructor(private apSerive: ApiService) {}
+
+  ngOnInit(): void {}
+
+  get isUserAuthenticated(): boolean {
+    return this.apSerive.isAuthenticatedUser();
+  }
 }
